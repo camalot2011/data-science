83e0f7f436fb62240f2b3c21dd71faccadbd1702
Zhandi Liao <zhandi.liao@gmail.com>
2018-04-20 17:13:54 -0700

    Assignment 2.2 Question 6

--- Python/Intro_Py_in_DataScience/Assignment2_2.py
+++ Python/Intro_Py_in_DataScience/Assignment2_2.py
@@ -1,6 +1,7 @@
 # Assignment 2.2
 
 import pandas as pd
+import numpy as np
 census_df = pd.read_csv('census.csv')
 census_df.head()
 
@@ -21,4 +22,19 @@ answer_five_one()
 
 #%% Question 6
 def answer_six():
-    county.groupby('STNAME')['CENSUS2010POP'].sort_values(ascending=False)
+    county = census_df.copy()
+    county = county[county['SUMLEV']==50]
+    county = county.set_index('STNAME')
+    popdata = county['CENSUS2010POP']
+    df = pd.DataFrame(columns = ['pop'])
+    for i in popdata.index.unique():
+        if np.prod(popdata.loc[i].shape) > 1:
+            descounties = popdata.loc[i].sort_values(ascending=False)
+            popfromtop3 = descounties[:3].sum()
+            df.loc[i] = [popfromtop3]
+        elif np.prod(popdata.loc[i].shape) == 1:
+            df.loc[i] = popdata.loc[i]
+    top3 = df.sort_values(by='pop',ascending=False)
+    return list(top3[:3].index)
+
+answer_six()
